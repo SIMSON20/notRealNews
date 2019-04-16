497e2877dcba4816c239f579b4cf3b1af88dc58c
jessiewx <tatuoo1117@gmail.com>
2019-04-16 14:30:09 +0800

    Update shuffle.py

--- python/shuffle.py
+++ python/shuffle.py
@@ -4,4 +4,4 @@ import sys
 for a in sys.argv:
     raw = pd.read_csv(a, header=None, sep = "\t")
     shuffled = raw.sample(frac = 1)
-    shuffled.to_csv(a.replace(".csv","_shuffled.csv"), header=False, sep = "\t")
+    shuffled.to_csv(a.replace(".csv","_shuffled.csv"), header=False, sep = "\t", index=False)
